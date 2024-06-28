c825c4d293d55c6e0ced9e1079741dd5b8b78929
MADHU2001228 <82486243+MADHU2001228@users.noreply.github.com>
2024-06-28 16:09:23 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,12 +1,8 @@
-
-def is_temperature_valid(temperature):
-    return 0 <= temperature <= 45
-
-def is_soc_valid(soc):
-    return 20 <= soc <= 80
+def is_in_range(value, lower, upper):
+    return lower <= value <= upper
 
 def battery_is_ok(temperature, soc, charge_rate):
-    if not is_temperature_valid(temperature) or not is_soc_valid(soc):
+    if not is_in_range(temperature, 0, 45) or not is_in_range(soc, 20, 80):
         print('Temperature or State of Charge is out of range!')
         return False
     elif charge_rate > 0.8:
@@ -17,4 +13,3 @@ def battery_is_ok(temperature, soc, charge_rate):
 if __name__ == '__main__':
     assert battery_is_ok(25, 70, 0.7) is True
     assert battery_is_ok(50, 85, 0) is False
-
