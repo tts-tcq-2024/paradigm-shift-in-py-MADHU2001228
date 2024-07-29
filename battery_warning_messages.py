dc24c24099b195b2af91773c974e199581ef640a
MADHU2001228 <82486243+MADHU2001228@users.noreply.github.com>
2024-07-29 08:34:12 +0530

    Update battery_warning_messages.py

--- battery_warning_messages.py
+++ battery_warning_messages.py
@@ -1,5 +1,6 @@
 def check_warning(value, min_value, max_value, tolerance, lower_warning, upper_warning, parameter_name):
     if min_value <= value < min_value + tolerance:
-        print(f"Warning: {lower_warning}")
+        print(f"Warning: {parameter_name} is {value}. {lower_warning}")
     elif max_value - tolerance < value <= max_value:
-        print(f"Warning: {upper_warning}")
+        print(f"Warning: {parameter_name} is {value}. {upper_warning}")
+
