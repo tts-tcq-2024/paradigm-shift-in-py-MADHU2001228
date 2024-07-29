1476a58d024ce5990b06922b819c3ed3b51ec47f
MADHU2001228 <82486243+MADHU2001228@users.noreply.github.com>
2024-07-29 08:26:06 +0530

    Create battery_monitor_main.py

---
+++ battery_monitor_main.py
@@ -0,0 +1,8 @@
+from check_limits import is_BatteryTemperature_Ok, is_BatterySoc_Ok, is_BatteryChargeRate_Ok
+
+def battery_Is_Ok(temperature, soc, chargeRate):
+    return is_BatteryTemperature_Ok(temperature) and is_BatterySoc_Ok(soc) and is_BatteryChargeRate_Ok(chargeRate)
+
+if __name__ == '__main__':
+    import battery_monitor_tests
+    battery_monitor_tests.run_tests()
